19f7d41c0200d1604a84f11b3db962e4717c1445
Noah <noahagreenman@gmail.com>
2025-04-01 11:24:06 -0400

    Deleted improper training data file and reuploaded to data directory along with raw data

--- Classification/Telco_Customer_Churn_Prediction/Customer_churn_app/main_app.py
+++ Classification/Telco_Customer_Churn_Prediction/Customer_churn_app/main_app.py
@@ -11,3 +11,9 @@ import numpy as np
 
 # Main page formatting
 st.title('Telco Customer Churn')
+
+# Download button (requires kaggle login)
+raw_data = pd.read_csv('https://github.com/greenmna/Data_Science_Portfolio/blob/main/Classification/Telco_Customer_Churn_Prediction/WA_Fn-UseC_-Telco-Customer-Churn.csv')
+training_data = pd.read_csv('https://github.com/greenmna/Data_Science_Portfolio/blob/main/Classification/Telco_Customer_Churn_Prediction/Customer_churn_app/training_dataframe.csv')
+
+st.dataframe(training_data)
