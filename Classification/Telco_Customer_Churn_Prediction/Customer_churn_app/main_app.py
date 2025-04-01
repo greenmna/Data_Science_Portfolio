d0ea2fc1580aa3156304257410a08f89857ae178
Noah <noahagreenman@gmail.com>
2025-04-01 11:29:06 -0400

    Modified telco notebook and removed extra index for training_dataframe.csv

--- Classification/Telco_Customer_Churn_Prediction/Customer_churn_app/main_app.py
+++ Classification/Telco_Customer_Churn_Prediction/Customer_churn_app/main_app.py
@@ -5,6 +5,7 @@ Source data: https://www.kaggle.com/datasets/blastchar/telco-customer-churn/data
 
 
 import streamlit as st
+import os
 import pandas as pd
 import numpy as np
 
@@ -13,7 +14,6 @@ import numpy as np
 st.title('Telco Customer Churn')
 
 # Download button (requires kaggle login)
-raw_data = pd.read_csv('https://github.com/greenmna/Data_Science_Portfolio/blob/main/Classification/Telco_Customer_Churn_Prediction/WA_Fn-UseC_-Telco-Customer-Churn.csv')
-training_data = pd.read_csv('https://github.com/greenmna/Data_Science_Portfolio/blob/main/Classification/Telco_Customer_Churn_Prediction/Customer_churn_app/training_dataframe.csv')
+training_data = pd.read_csv(os.path.join(os.path.dirname(__file__), 'data', 'training_dataframe.csv'))
 
 st.dataframe(training_data)
