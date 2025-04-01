bceb733c317ff102129879e1a590163583db3d9e
Noah <noahagreenman@gmail.com>
2025-04-01 13:42:46 -0400

    Integrated model into churn prediction page

--- Classification/Telco_Customer_Churn_Prediction/Customer_churn_app/pages/1_Churn_prediction.py
+++ Classification/Telco_Customer_Churn_Prediction/Customer_churn_app/pages/1_Churn_prediction.py
@@ -1,9 +1,25 @@
 """Page for inputting customer data that model uses to predict churn"""
 
 import streamlit as st
+import pickle
+import os
+import xgboost
 
+# Required first code in separate pages
 st.set_page_config(page_title='Customer Churn Predictor', 
                    page_icon='❓')
+
+# Load-in model
+@st.cache_resource
+def churn_prediction_model():
+    parent_dir = os.path.dirname(os.path.abspath(__file__))
+    model_dir = os.path.join(parent_dir, '..', 'model', 'Telco_customer_churn_model.pkl')
+    with open(model_dir, 'rb') as file:
+        model = pickle.load(file)
+    return model
+
+
+model = churn_prediction_model()
 
 # Page layout
 st.markdown('# Customer Churn Predictor')
